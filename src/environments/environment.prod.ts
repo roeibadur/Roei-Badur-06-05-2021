a9f146ca88ca755e2b4a191e6b82a0de9c397f6a
roeibadur <51096879+roeibadur@users.noreply.github.com>
2021-05-10 18:36:01 +0300

    add https for deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   ApiKey: '1hLxMiBAf1j5ksoJ0oYgbU5OSGDryrbZ',
-  address: 'http://dataservice.accuweather.com/'
+  address: 'https://dataservice.accuweather.com/'
 };
